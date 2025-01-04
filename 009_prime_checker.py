13cef89ec0de32e3559e2d19270611b3d298e2db
Ernest "Ernie Bird" Deriabin <ernest.deriabin@gmail.com>
2025-01-04 15:31:35 -0500

    Update 009_prime_checker.py

--- 009_prime_checker.py
+++ 009_prime_checker.py
@@ -1,3 +1,13 @@
+def primecheck(n):
+    if n < 2:
+        return False
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+    return True
+
+#---------without return-------
+
 def is_prime(n):
     if n < 2:
         print("Not Prime")
